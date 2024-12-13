4a3b199d8d0e68816e0bc789dd86d7b049e197fa
unknown <mustafasmart671st@gmail.com>
2024-12-13 11:08:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const page = gsap.timeline();
+
+page.from('.image', {
+    opacity: 0,
+    duration: 2,
+    y: 100
+})
+page.from('.text h2', {
+    opacity: 0,
+    duration: 1,
+    y: 100
+})
+page.from('header', {
+    opacity: 0,
+    duration: 1,
+    y: -100,
+    delay: .5
+})
+page.from('.links a', {
+    opacity: 0,
+    duration: 1,
+    y: -50,
+    stagger: .3
+})
+page.from('.logo', {
+    opacity: 0,
+    duration: 1,
+    x: -50,
+})
+page.from('.btn', {
+    opacity: 0,
+    duration: 1,
+    x: -50,
+})
+
